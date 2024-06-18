be209e7824bf6427504c8e15086736c365a26a6a
austernotus <76696499+austernotus@users.noreply.github.com>
2024-06-18 15:11:31 +0100

    loaders configs

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,8 @@ module.exports = {
           'style-loader',
           'css-loader',
         ],
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
