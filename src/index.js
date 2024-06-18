bff1207fcd47ef032017ce6e9eb06f6903d19216
austernotus <76696499+austernotus@users.noreply.github.com>
2024-06-18 13:29:08 +0100

    fixed logic to change content on page

--- src/index.js
+++ src/index.js
@@ -3,26 +3,28 @@ import {menu} from "./menu";
 import {about} from "./about";
 import "./styles.css";
 
-const contentDiv = document.querySelector("#content")
-contentDiv.innerHTML = home;
+const contentDiv = document.querySelector("#content");
 
 const homeButton = document.querySelector("#home-button");
 const menuButton = document.querySelector("#menu-button");
 const aboutButton = document.querySelector("#about-button");
-const enterButton = document.querySelector("#enter-button");
 
-function homeScreen(){
+function homeScreen() {
     contentDiv.innerHTML = home;
+    const enterButton = document.querySelector("#enter-button");
+    enterButton.addEventListener("click", menuScreen);
 }
-function menuScreen(){
+
+function menuScreen() {
     contentDiv.innerHTML = menu;
 }
-function aboutScreen(){
+
+function aboutScreen() {
     contentDiv.innerHTML = about;
 }
 
-homeButton.addEventListener("click", homeScreen)
-menuButton.addEventListener("click", menuScreen)
-aboutButton.addEventListener("click", aboutScreen)
-enterButton.addEventListener("click", menuScreen)
+homeButton.addEventListener("click", homeScreen);
+menuButton.addEventListener("click", menuScreen);
+aboutButton.addEventListener("click", aboutScreen);
 
+homeScreen()
